1a4b02181584792dfaea26c8db395c8d2e32d016
Emmadsadiq01 <junaid0316>
2020-11-23 21:46:53 +0500

    WebTemplate

--- contactform.php
+++ contactform.php
@@ -8,7 +8,7 @@ if (isset($_POST['submit'])){
     $message = $_POST['msg'];
 
     $mailto = "ovais@rentmemedia.com";
-    $headers = "Website URL: ".$website.".\n\n".$message;
+    $headers = ".Website URL: ".$website.".\n\n".$message.".\n\nEmail: ".$mailfrom.".\nNumber: ".$number;
     $txt = "Mail From ".$name.".\n\n";
    
     mail($mailto, $txt, $headers);
